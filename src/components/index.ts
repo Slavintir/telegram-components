61dce1ac5c29f56401bfab8fd12556f42f8b0b29
vyacheslav <govras99@outlook.com>
2020-11-19 11:26:31 +0200

    updated

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { KeyboardComponent, ButtonData, KeyboardState } from './keyboard';
 export { ButtonComponent, ButtonState } from './button';
+export { PictureComponent, PictureState } from './picture';
